2ebac46a4687a083246659f49dde57a8920ef640
Harsh Jain <hj19062001@gmail.com>
2024-10-02 19:31:53 +0530

    commit

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
         // console.log('Connection Instance: ');
         // console.log(connectionInstance);
-        console.log(`\n MOngoDB connected!! DB HOST: ${connectionInstance.connection.host}`)
+        console.log(`\n MongoDB connected!! DB HOST: ${connectionInstance.connection.host}`)
     } catch (error) {
         console.error("MongoDB connection Failed!: ", error);
         process.exit(1);
